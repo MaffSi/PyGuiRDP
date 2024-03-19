961f882f0a52de2ee9d25e00b22c9e6ac6e64ae3
MaffSi <mateuscostadesousa37@gmail.com>
2024-03-18 22:29:22 -0300

    Add comments, config option, server save and read option

--- main.py
+++ main.py
@@ -1,25 +1,70 @@
+# Importação dos módulos necessários
 import sys
 import os
 import subprocess
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QInputDialog
 from PyQt5 import uic, QtCore
+import configparser
 
+# Função para criar um arquivo de configuração com o endereço do servidor RDP
+def createConf(rdp_server):
+    # Cria um objeto ConfigParser
+    configuration_file = configparser.ConfigParser()
+    # Adiciona o endereço do servidor RDP à seção 'General' do arquivo de configuração
+    configuration_file['General'] = {'RdpServer':rdp_server}
+    
+    # Escreve as configurações no arquivo config.ini
+    with open('config.ini','w') as configfile:
+        configuration_file.write(configfile)
+
+# Função para ler o endereço do servidor RDP do arquivo de configuração
+def readConf(config_file):
+    # Cria um objeto ConfigParser
+    configuration_file = configparser.ConfigParser()  
+    # Lê o arquivo de configuração config.ini
+    configuration_file.read('config.ini')
+    # Retorna o endereço do servidor RDP armazenado na seção 'General'
+    return configuration_file.get('General','RdpServer')
+
+# Classe da janela de login RDP
 class RDPLoginWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.load_ui()
 
+    # Carregar a interface do usuário
     def load_ui(self):
+        # Carrega a interface de usuário definida no arquivo LogIn.ui
         uic.loadUi("LogIn.ui", self)
+        # Preenche o campo de endereço do servidor com o valor lido do arquivo de configuração
+        self.ServidorURL.setText(readConf('config.ini'))
+        # Conecta os sinais de pressionar 'Enter' nos campos de texto aos métodos correspondentes
         self.Password.returnPressed.connect(self.on_login_button_clicked)
+        # Conecta o sinal de clique no botão 'Entrar' ao método correspondente
         self.Entrar.clicked.connect(self.on_login_button_clicked)
+        # Conecta o sinal de clique no botão 'Config' ao método correspondente
+        self.Config.clicked.connect(self.config_ui)
 
+    # Configurar a interface do usuário para inserir o endereço do servidor
+    def config_ui(self):
+        # Abre um diálogo para inserir o endereço do servidor
+        url, ok = QInputDialog().getText(self,'Configuração', 'Insira o endereço do servidor:')
+        # Se o usuário inseriu um endereço e confirmou, atualiza o campo de endereço do servidor e cria/atualiza o arquivo de configuração
+        if ok:
+            self.ServidorURL.setText(str(url))
+            createConf(str(url))
+            
+    
+    # Conectar-se ao servidor RDP
     def connect_to_rdp(self, username, password):
-        # Replace placeholders with your RDP server address and username
-        rdp_server = os.environ.get('RdpServer','')
+        # Verifica se o arquivo de configuração existe e lê o endereço do servidor RDP dele
+        if os.path.exists('config.ini'):
+            rdp_server = readConf('config.ini')
+        else:
+            rdp_server = ''
         rdp_username = username
 
-        # Construct xfreerdp command with provided username and password
+        # Constrói o comando xfreerdp com o endereço do servidor, nome de usuário e senha fornecidos
         command = [
             "xfreerdp",
             "/v:" + rdp_server,
@@ -27,35 +72,49 @@ class RDPLoginWindow(QMainWindow):
             "/p:" + password,
 	    "/dynamic-resolution",
             "/printer",
-            "/cert-ignore",  # Ignore certificate errors (optional)
+            "/cert-ignore",  # Ignorar erros de certificado (opcional)
 	    "+aero",
             "+clipboard"
         ]
 
-        # Launch xfreerdp process
+        # Inicia o processo xfreerdp
         try:
             subprocess.run(command, check=True)
         except subprocess.CalledProcessError as e:
-            # Handle any errors occurred during the connection
+            # Exibe uma mensagem de erro em caso de falha na conexão
             QMessageBox.critical(None, "Error", f"Failed to connect: {e}")
 
+    # Lidar com o clique no botão de login
     def on_login_button_clicked(self):
+        # Obtém o nome de usuário e senha digitados pelo usuário
         username = self.User.text()
         password = self.Password.text()
 
+        # Se ambos os campos de nome de usuário e senha estiverem preenchidos
         if username and password:
+            # Conecta-se ao servidor RDP com as credenciais fornecidas
             self.connect_to_rdp(username, password)
+            # Limpa os campos de nome de usuário e senha após a tentativa de login
             self.User.clear()
             self.Password.clear()
         else:
-            QMessageBox.warning(None, "Warning", "Please enter both username and password.")
+            # Se um ou ambos os campos estiverem vazios, exibe uma mensagem de aviso
+            QMessageBox.warning(None, "Warning", "Insira seu nome de usuário e senha!")
 
+# Função principal
 def main():
+    # Inicia a aplicação PyQt5
     app = QApplication(sys.argv)
+    # Cria uma instância da janela de login RDP
     window = RDPLoginWindow()
+    # Oculta a barra de título da janela
     window.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+    # Exibe a janela de login
     window.show()
+    # Executa o loop de eventos da aplicação
     sys.exit(app.exec())
 
+# Verificar se o script está sendo executado como o programa principal
 if __name__ == "__main__":
+    # Chama a função principal para iniciar o aplicativo
     main()
